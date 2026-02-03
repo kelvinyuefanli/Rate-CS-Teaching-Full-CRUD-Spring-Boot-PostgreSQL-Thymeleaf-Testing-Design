f1b75ec2d89c7e5ac25cbe1c27a8e4183b2387a0
Kelvin Li <kelvin@u172-016-106-233.wireless.sfu.ca>
2026-02-03 15:17:31 -0800

    chore: bootstrap Spring Boot scaffold
    Set up Maven wrapper, base app entrypoint, and application config
    for local Postgres usage.

---
+++ src/main/java/ca/sfu/cmpt276/staffrating/StaffRatingApplication.java
@@ -0,0 +1,11 @@
+package ca.sfu.cmpt276.staffrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffRatingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StaffRatingApplication.class, args);
+    }
+}
